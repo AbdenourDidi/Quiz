85ebafb1104691f4d904232672e226851d5d99d5
AbdenourDidi <abdenour.didi@student.vinci.be>
2025-01-25 20:29:43 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,5 @@ app.get("/api/quiz", async (req, res) => {
 module.exports = app;
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
