bfff87a2122947ac6543e7037ef7707cb67da6f8
AbdenourDidi <abdenour.didi@student.vinci.be>
2025-01-25 19:18:17 +0100

    refactor

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
-app.get("/", () => {
+app.get("/", (res) => {
   res.json("hello");
 });
 
