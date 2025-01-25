8c7bba1f6a59d6c13dc0d995e8e52b6a472ccd2f
AbdenourDidi <abdenour.didi@student.vinci.be>
2025-01-25 18:49:22 +0100

    refactor

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 // Connect to MongoDB
